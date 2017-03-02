77f457d901510ccc984af55dcbabc3d7075182b2
Ivan <kuzmenko.ivan2002@yandex.com>
2017-03-02 22:18:00 +0300

    Example

--- nushdiki.js
+++ nushdiki.js
@@ -1,1 +1,13 @@
 // Fit ha!
+
+var fit = ["Fit ", "Жили у бабуси два весёлых гуся. Один серый, другой белый, "];
+var ha = ["HA!", "оба голубые!"];
+
+function getRandomInt(max) {
+  	return Math.floor(Math.random() * (max + 1));
+}
+
+function random_nushdik(playSound) {
+	playSound();
+	return fit[getRandomInt(fit.length - 1)] + ha[getRandomInt(ha.length - 1)];
+}
