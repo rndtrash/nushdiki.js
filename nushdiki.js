ccf8c211ec2dfacefef8e12e20aabb18d77f02bd
Ivan <kuzmenko.ivan2002@yandex.com>
2017-03-03 09:53:51 +0300

    Увеличил базу нуждиков

--- nushdiki.js
+++ nushdiki.js
@@ -1,13 +1,12 @@
 // Fit ha!
 
-var fit = ["Fit ", "Жили у бабуси два весёлых гуся. Один серый, другой белый, "];
-var ha = ["HA!", "оба голубые!"];
+var fit = ["Фьить-", "Жили у бабуси два весёлых гуся. Один серый, другой белый, ", "Не бывает некрасивых надувных женщин, ", "Ещё не вечер, ", "Что вы хотите от нашей эстрады, "];
+var ha = ["ХА!", "оба голубые!", "бывают слабые легкие.", "а уже как утром.", "если настоящие мужчины играют в хоккей."];
 
 function getRandomInt(max) {
   	return Math.floor(Math.random() * (max + 1));
 }
 
-function random_nushdik(playSound) {
-	playSound();
+function random_nushdik() {
 	return fit[getRandomInt(fit.length - 1)] + ha[getRandomInt(ha.length - 1)];
 }
